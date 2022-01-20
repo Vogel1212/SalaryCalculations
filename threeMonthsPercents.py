cdc4e428933ed2b4a63b44400625f25bf8fc1a93
Gabriel Vogel <vogel.gabriel@hotmail.com>
2022-01-19 23:54:04 -0300

    Update threeMonthsPercents.py

--- threeMonthsPercents.py
+++ threeMonthsPercents.py
@@ -2,27 +2,30 @@
 # from math import sqrt 
 
 class Calculator:
-    def __init__(self, sal, perc, months, total, calcfim):
+    def __init__(self, sal, perc, months, total, calcfim, result):
         self.sal = sal
         self.perc = perc
         self.months = months
         self.calcfim = calcfim
+        self.result = result
+    
+    # def Calculation(self):
+    #     calc = sal*(perc/100)
+    #     totalSal = calc+sal
     
     def Calculation(self):
-        calc = sal*(perc/100)
-        totalSal = calc+sal
-    
+        result = sal + (sal * 0.15)
         
 sal = int(input('What is the employees monthly salary? US$'))
 perc = int(input('How many percent quarterly increase?: '))
 months = int(input('How many months should this calculation take?: '))
 
 if months == 1:
-    totalSal = Calculator.totalSal
+    totalSal = Calculator.result
 elif months == 2:
-    totalSal = Calculator.totalSal*2
+    totalSal = Calculator.result*2
 
 Calculator()
 
 print('The employees old salary was US${:.2f}, now with {}% of quarterly increase during {} years, will start to win US${:.2f} a month.\nWith an increase of US${:.2f} after 3 months'.format
-      (sal,perc,months,totalSal,Calculator.calcfim,Calculator.totalSal))
+      (sal,perc,months,totalSal,Calculator.calcfim,Calculator.result))
